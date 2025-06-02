4a32fcbd1bc31f23766431f70063ef2ebcfcb6e2
ramjan88 <gmramjanali8888@gmail.com>
2025-06-02 09:44:55 +0600

    setup career code server site

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+// career-code-db
+// JHINzZ0jLojxMyOP
+
+const uri = `mongodb+srv://${process.env.CAREER_CODE_USERNAME}:${process.env.CAREER_CODE_PASSWORD}@brainbazzdatabase.ihuzu7m.mongodb.net/?retryWrites=true&w=majority&appName=BrainBazzDatabase`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const careerCollection=client.db('CareerDB').collection('jobs')
+
+
+    app.get('/jobs',async(req,res)=>{
+      const result=await careerCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/jobs/:id',async(req,res)=>{
+      const id =req.params.id
+      const query={_id: new ObjectId(id)}
+      const result=await careerCollection.findOne(query)
+      res.send(result)
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("career code server is cooking");
+});
+
+app.listen(port, () => {
+  console.log(`career server is running port ${port}`);
+});
